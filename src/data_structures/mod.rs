7f1753ccad736641b0d19f4e59972ef49c329444
pgegh <hovig@mail.com>
2021-10-03 15:50:39 +0200

    Add a copyright notice to all .rs files

--- src/data_structures/mod.rs
+++ src/data_structures/mod.rs
@@ -1,3 +1,20 @@
+// Copyright © 2021 Hovig Manjikian
+//
+// This file is part of json.
+//
+// json is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// json is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with json.  If not, see <https://www.gnu.org/licenses/>.
+
 mod j_string;
 mod ws;
 
