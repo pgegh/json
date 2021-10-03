a5af72d606bd4deae16bfb18e6f562f18362b886
hovig <hovig@mail.com>
2021-10-03 15:02:02 +0200

    Update documentation for the whitespace data-structure

--- src/data_structures/mod.rs
+++ src/data_structures/mod.rs
@@ -1,8 +1,10 @@
 mod j_string;
+
 pub use j_string::JString;
 
-/// A data-structure that represents white space as defined in the ECMA-404 standard.
-/// The allowed characters are 0x20, 0x0A, 0x0D, 0x09.
+/// A data-structure that represents whitespace. Whitespace is any sequence of one or more of
+/// the following code points: character tabulation (U+0009), line feed (U+000A), carriage return
+/// (U+000D), and space (U+0020).
 #[derive(Debug, Clone)]
 pub struct Ws {
     value: String,
@@ -15,17 +17,17 @@ impl Ws {
     /// use json::data_structures::Ws;
     ///
     /// let mut white_space = String::new();
-    /// white_space.push(0x20 as char);
-    /// white_space.push(0x0A as char);
-    /// white_space.push(0x0D as char);
-    /// white_space.push(0x09 as char);
+    /// white_space.push(0x0020 as char);
+    /// white_space.push(0x000A as char);
+    /// white_space.push(0x000D as char);
+    /// white_space.push(0x0009 as char);
     ///
     /// let ws = Ws::new(white_space.clone()).unwrap();
     ///
     /// assert_eq!(white_space, ws.to_string());
     ///
     /// // Testing with an illegal string
-    /// white_space.push(0x0B as char);
+    /// white_space.push(0x000B as char);
     ///
     /// assert_eq!(Err("The string contains illegal characters"), Ws::new(white_space));
     /// ```
