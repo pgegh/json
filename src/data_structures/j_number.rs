84be199e305e0c277f31fb1e2b168a2e07256809
pgegh <hovig@mail.com>
2021-10-08 13:44:00 +0200

    Refactor json::data_structures::Number number.rs to j_number.rs json::data_structures::Number to json::data_structures::JNumber

--- src/data_structures/j_number.rs
+++ src/data_structures/j_number.rs
@@ -23,7 +23,7 @@ use std::str::FromStr;
 /// point (U+002E).It may have an exponent, prefixed by e(U+0065) or E(U+0045) and
 /// optionally +(U+002B) or –(U+002D). The digits are the code points U+0030 through U+0039.
 #[derive(Debug, Clone)]
-pub struct Number {
+pub struct JNumber {
     sign: Sign,
     integer_part: String,
     fractional_part: String,
@@ -33,14 +33,14 @@ pub struct Number {
     f64_value: f64,
 }
 
-impl Number {
+impl JNumber {
     /// returns an f64 representation of the given number
     pub fn get_f64_value(&self) -> f64 {
         self.f64_value
     }
 }
 
-impl Display for Number {
+impl Display for JNumber {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}{}{}{}{}{}{}",
                if self.sign == Sign::Negative { "-" } else { "" },
@@ -54,25 +54,25 @@ impl Display for Number {
     }
 }
 
-impl FromStr for Number {
+impl FromStr for JNumber {
     type Err = String;
 
     /// Will create a Number form a string
     ///
     /// ```
     /// use std::str::FromStr;
-    /// use json::data_structures::Number;
+    /// use json::data_structures::JNumber;
     ///
-    /// let n = Number::from_str("-0.001").unwrap();
+    /// let n = JNumber::from_str("-0.001").unwrap();
     /// assert_eq!("-0.001".to_string(), n.to_string());
     /// assert_eq!(-0.001_f64, n.get_f64_value());
     ///
-    /// let n = Number::from_str("2.5e7").unwrap();
+    /// let n = JNumber::from_str("2.5e7").unwrap();
     /// assert_eq!("2.5e7".to_string(), n.to_string());
     /// assert_eq!(25000000.0_f64, n.get_f64_value());
     ///
     /// // Illegal case
-    /// let n = Number::from_str("2.5.000");
+    /// let n = JNumber::from_str("2.5.000");
     /// assert_eq!(Err("An illegal point at index 3".to_string()), n);
     /// ```
     // Todo: High cyclomatic complexity! Optimization needed.
@@ -160,7 +160,7 @@ impl FromStr for Number {
                                 &fractional_part,
                                 &e_sign,
                                 &exponent_part);
-        Ok(Number {
+        Ok(JNumber {
             sign,
             integer_part,
             fractional_part,
@@ -172,7 +172,7 @@ impl FromStr for Number {
     }
 }
 
-impl PartialEq for Number {
+impl PartialEq for JNumber {
     fn eq(&self, other: &Self) -> bool {
         self.f64_value == other.f64_value
     }
@@ -210,101 +210,101 @@ pub enum Sign {
 #[cfg(test)]
 mod test {
     use std::str::FromStr;
-    use crate::data_structures::Number;
+    use crate::data_structures::JNumber;
 
     #[test]
     fn test_zero() {
-        let n = Number::from_str("0").unwrap();
+        let n = JNumber::from_str("0").unwrap();
         assert_eq!("0".to_string(), n.to_string());
         assert_eq!(0_f64, n.get_f64_value());
 
-        let n = Number::from_str("-0").unwrap();
+        let n = JNumber::from_str("-0").unwrap();
         assert_eq!("-0".to_string(), n.to_string());
         assert_eq!(-0_f64, n.get_f64_value());
     }
 
     #[test]
     fn test_legal_negative_int() {
-        let n = Number::from_str("-300").unwrap();
+        let n = JNumber::from_str("-300").unwrap();
         assert_eq!("-300".to_string(), n.to_string());
         assert_eq!(-300_f64, n.get_f64_value());
     }
 
     #[test]
     fn test_legal_positive_int() {
-        let n = Number::from_str("5898499948554533445").unwrap();
+        let n = JNumber::from_str("5898499948554533445").unwrap();
         assert_eq!("5898499948554533445".to_string(), n.to_string());
         assert_eq!(5898499948554533445_f64, n.get_f64_value());
     }
 
     #[test]
     fn test_legal_negative_decimal() {
-        let n = Number::from_str("-0.0016387").unwrap();
+        let n = JNumber::from_str("-0.0016387").unwrap();
         assert_eq!("-0.0016387".to_string(), n.to_string());
         assert_eq!(-0.0016387_f64, n.get_f64_value());
     }
 
     #[test]
     fn test_legal_positive_decimal() {
-        let n = Number::from_str("340.600").unwrap();
+        let n = JNumber::from_str("340.600").unwrap();
         assert_eq!("340.600".to_string(), n.to_string());
         assert_eq!(340.600_f64, n.get_f64_value());
     }
 
     #[test]
     fn test_legal_negative_exponent() {
-        let n = Number::from_str("-0.0016387e7").unwrap();
+        let n = JNumber::from_str("-0.0016387e7").unwrap();
         assert_eq!("-0.0016387e7".to_string(), n.to_string());
         assert_eq!(-0.0016387e7_f64, n.get_f64_value());
 
-        let n = Number::from_str("-0.0016387E-3").unwrap();
+        let n = JNumber::from_str("-0.0016387E-3").unwrap();
         assert_eq!("-0.0016387E-3".to_string(), n.to_string());
         assert_eq!(-0.0016387e-3_f64, n.get_f64_value());
 
-        let n = Number::from_str("-0.0016387E+3").unwrap();
+        let n = JNumber::from_str("-0.0016387E+3").unwrap();
         assert_eq!("-0.0016387E+3".to_string(), n.to_string());
         assert_eq!(-0.0016387e+3_f64, n.get_f64_value());
     }
 
     #[test]
     fn test_legal_positive_exponent() {
-        let n = Number::from_str("340.6001e5").unwrap();
+        let n = JNumber::from_str("340.6001e5").unwrap();
         assert_eq!("340.6001e5".to_string(), n.to_string());
         assert_eq!(340.6001e5_f64, n.get_f64_value());
 
-        let n = Number::from_str("340.6001E-5").unwrap();
+        let n = JNumber::from_str("340.6001E-5").unwrap();
         assert_eq!("340.6001E-5".to_string(), n.to_string());
         assert_eq!(340.6001e-5_f64, n.get_f64_value());
 
-        let n = Number::from_str("340.6001E+2").unwrap();
+        let n = JNumber::from_str("340.6001E+2").unwrap();
         assert_eq!("340.6001E+2".to_string(), n.to_string());
         assert_eq!(340.6001e+2_f64, n.get_f64_value());
     }
 
     #[test]
     fn test_illegal_numbers() {
-        let n = Number::from_str("00");
+        let n = JNumber::from_str("00");
         assert_eq!(Err("Illegal input! Point was expected at index 1".to_string()), n);
 
-        let n = Number::from_str("+0");
+        let n = JNumber::from_str("+0");
         assert_eq!(Err("Illegal symbol + at index 0".to_string()), n);
 
-        let n = Number::from_str(".30");
+        let n = JNumber::from_str(".30");
         assert_eq!(Err("Illegal symbol . at index 0".to_string()), n);
 
-        let n = Number::from_str("1.3.0");
+        let n = JNumber::from_str("1.3.0");
         assert_eq!(Err("An illegal point at index 3".to_string()), n);
 
-        let n = Number::from_str("1-30");
+        let n = JNumber::from_str("1-30");
         assert_eq!(Err("An illegal sign at index 1".to_string()), n);
 
-        let n = Number::from_str("-13e2.3");
+        let n = JNumber::from_str("-13e2.3");
         assert_eq!(Err("An illegal point at index 5".to_string()), n);
 
-        let n = Number::from_str("13e2e3");
+        let n = JNumber::from_str("13e2e3");
         assert_eq!(Err("Illegal symbol e at index 4".to_string()), n);
 
-        let n = Number::from_str("-1123.35E2E3");
+        let n = JNumber::from_str("-1123.35E2E3");
         assert_eq!(Err("Illegal symbol E at index 10".to_string()), n);
     }
 }
